66b1ba72271ab4f33cdf285c0f7fa9d97b661f02
AsGinku_1976 <aginku@yandex.ru>
2022-05-24 21:29:07 +0300

    Initial commit

---
+++ src/main/kotlin/ru/netelogy/main.kt
@@ -0,0 +1,5 @@
+package ru.netelogy
+
+fun main(){
+    TODO()
+}
---
+++ src/main/kotlin/ru/netelogy/service/WallService.kt
@@ -0,0 +1,14 @@
+package ru.netelogy.service
+
+import ru.netelogy.data.Post
+
+object WallService {
+
+    fun add(post: Post): Post {
+        TODO()
+    }
+
+    fun update(post: Post): Boolean {
+        TODO()
+    }
+}
