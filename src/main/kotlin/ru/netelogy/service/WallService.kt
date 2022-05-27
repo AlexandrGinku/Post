c31b1f01cd323f01e781fc0eea1d96807361e5eb
AsGinku_1976 <aginku@yandex.ru>
2022-05-27 23:45:40 +0300

    add WallService

--- src/main/kotlin/ru/netelogy/service/WallService.kt
+++ src/main/kotlin/ru/netelogy/service/WallService.kt
@@ -3,12 +3,23 @@ package ru.netelogy.service
 import ru.netelogy.data.Post
 
 object WallService {
+    private var posts = emptyArray<Post>()
+    private var lastId = 0
 
     fun add(post: Post): Post {
-        TODO()
+        lastId ++
+        val newPost = post.copy(id = lastId)
+        posts += newPost
+        return posts.last()
     }
 
     fun update(post: Post): Boolean {
-        TODO()
+        for ((index, item) in posts.withIndex()) {
+            if (item.id == post.id) {
+                posts[index] = post
+                return true
+            }
+        }
+        return false
     }
 }
